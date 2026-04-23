180e608b51762afee9bb53dfb712ce0f6a482a55
bij2706-byte <biakhtarzandidas@gmail.com>
2026-04-22 20:21:14 -0400

    added styling to app

--- app.js
+++ app.js
@@ -23,18 +23,60 @@ http.createServer(function (req, res) {
     /* HOME PAGE */
     if (urlObj.pathname == "/") {
 
-        res.write("<h2>Stock Search App</h2>");
+        res.write(`
+        <html>
+        <head>
+        <title>Stock App</title>
+        <style>
+        body {
+            font-family: Arial;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
 
-        var form = "<form method='get' action='/process'>" +
-                   "Enter company or ticker: <input type='text' name='search'><br><br>" +
+        .container {
+            background: white;
+            padding: 20px;
+            margin: 100px auto;
+            width: 300px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px gray;
+        }
 
-                   "<input type='radio' name='type' value='company' checked> Company" +
-                   "<input type='radio' name='type' value='ticker'> Ticker<br><br>" +
+        input[type=text] {
+            width: 90%;
+            padding: 8px;
+            margin: 10px 0;
+        }
 
-                   "<input type='submit'>" +
-                   "</form>";
+        button {
+            padding: 10px;
+            background: #007BFF;
+            color: white;
+            border: none;
+            border-radius: 5px;
+        }
+        </style>
+        </head>
 
-        res.write(form);
+        <body>
+        <div class="container">
+        <h2>Stock Search</h2>
+
+        <form method="get" action="/process">
+            <input type="text" name="search" placeholder="Enter company or ticker"><br>
+
+            <input type="radio" name="type" value="company" checked> Company
+            <input type="radio" name="type" value="ticker"> Ticker<br><br>
+
+            <button type="submit">Search</button>
+        </form>
+
+        </div>
+        </body>
+        </html>
+        `);
+
         res.end();
     }
 
@@ -57,7 +99,6 @@ http.createServer(function (req, res) {
 
             var query;
 
-            // determine search type
             if (type == "company") {
                 query = { name: search };
             } else {
@@ -72,21 +113,50 @@ http.createServer(function (req, res) {
                     return;
                 }
 
-                res.write("<h2>Results:</h2>");
+                res.write(`
+                <html>
+                <head>
+                <style>
+                body {
+                    font-family: Arial;
+                    background-color: #f4f4f4;
+                    text-align: center;
+                }
+
+                .result-box {
+                    background: white;
+                    margin: 20px auto;
+                    padding: 15px;
+                    width: 300px;
+                    border-radius: 10px;
+                    box-shadow: 0px 0px 10px gray;
+                }
+                </style>
+                </head>
+
+                <body>
+                <h2>Results</h2>
+                `);
 
                 if (results.length == 0) {
-                    res.write("No results found");
+                    res.write("<p>No results found</p>");
                 } else {
 
                     for (var i = 0; i < results.length; i++) {
 
                         console.log(results[i]);
 
-                        res.write("Name: " + results[i].name + "<br>");
-                        res.write("Ticker: " + results[i].ticker + "<br>");
-                        res.write("Price: " + results[i].price + "<br><br>");
+                        res.write(`
+                        <div class="result-box">
+                        Name: ${results[i].name}<br>
+                        Ticker: ${results[i].ticker}<br>
+                        Price: ${results[i].price}
+                        </div>
+                        `);
                     }
                 }
+
+                res.write("</body></html>");
 
                 res.end();
                 client.close();
